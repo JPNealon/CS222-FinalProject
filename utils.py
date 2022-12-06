c3448329b895e8a2dbd61b133da33c797704b0cf
Jack Nealon <jnealon0805@gmail.com>
2022-12-06 11:47:46 -0800

    save

--- utils.py
+++ utils.py
@@ -20,3 +20,10 @@ temp3_df = pd.read_json("RyderStreamHist.json")
 print(temp3_df)
 temp3_df[["temp1", "temp2"]] = temp3_df.artist.str.split(expand=True)
 print(temp3_df)'''
+
+'''
+To-do cleaning:
+1. Add the artist's genre (using Dr. Sprints spotify artist-genre program/function)
+1. Add a season column (use the date to correlate it's season)
+1. Date the song/podcast was released ()
+'''
