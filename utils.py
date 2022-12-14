3ae7f351b6c198524fe9dc1ab7ae1a37ee095310
Jack Nealon <jnealon0805@gmail.com>
2022-12-13 23:11:53 -0800

    save

--- utils.py
+++ utils.py
@@ -32,6 +32,7 @@ def clean_jack_info(jack_df):
     jack_df['msPlayed'] = jack_df['msPlayed'] / 1000
     jack_df[['Year','Month','Day']] = jack_df.Date.str.split(pat='-', expand=True)
     jack_df = jack_df.drop('Date', axis=1)
+    jack_df['artistName'] = jack_df['artistName'].str.replace("Andrew Schulz's Flagrant with Akaash Singh", 'AS Flagrant Podcast')
     return jack_df
 
 def clean_ryder_info(ryder_df):
@@ -72,14 +73,12 @@ def graph_jack_monthly_plays(jack_df):
     plt.bar(month_df.index, month_df)
     plt.xticks(ha="right", rotation=45)
     plt.title("Jack - Total Monthly Plays")
-    plt.xlabel("Date (2021-2022)")
+    plt.xlabel("Month (2021-2022)")
     plt.ylabel("Number of Songs Played")
     plt.tight_layout()
 
 def graph_jack_favorite_artists(jack_df):
     artists_df = jack_df["artistName"].value_counts()
-    print(artists_df[0:10])
-    artists_df = artists_df.drop(["Andrew Schulz's Flagrant with Akaash Singh"], axis=0)
     plt.bar(artists_df.index[0:24], artists_df[0:24])
     plt.xticks(rotation=45, ha="right")
     plt.title("Jack - Artist's total songs played")
@@ -92,7 +91,7 @@ def graph_ryder_monthly_plays(ryder_df):
     plt.bar(month_df.index, month_df)
     plt.xticks(ha="right", rotation=45)
     plt.title("Ryder - Total Monthly Plays")
-    plt.xlabel("Date (2022)")
+    plt.xlabel("Month (2022)")
     plt.ylabel("Number of Songs Played")
     plt.tight_layout()
 
